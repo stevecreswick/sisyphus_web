d9301f63391f352583e973cd8d6688f5942d5476
Steve Creswick <screswick@gmail.com>
2018-02-17 19:54:30 -0500

    Add base folders for api interaction

---
+++ src/web/javascripts/reducers/index.js
@@ -0,0 +1,17 @@
+// import { combineReducers } from 'redux';
+// import { reducer as form } from 'redux-form';
+// // import session from './session';
+// import rocks from './rooms';
+//
+// const appReducer = combineReducers({
+//   form,
+//   // session,
+//   rooms
+// });
+//
+// export default function (state, action) {
+//   // if (action.type === 'LOGOUT') {
+//   //   return appReducer(undefined, action);
+//   // }
+//   return appReducer(state, action);
+// }
